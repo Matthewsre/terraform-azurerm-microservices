1c5be9abb7d49cbcc943a0b72429d2fa9c512f5d
Matt Sanders <matthewsre@hotmail.com>
2021-02-02 16:32:00 -0800

    Added required provider version

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 0.14"
   experiments      = [module_variable_optional_attrs]
+
+  required_providers {
+    azurerm = {
+      version = ">= 2.0.0"
+      source  = "hashicorp/azurerm"
+    }
+  }
 }
 
 provider "azurerm" {
