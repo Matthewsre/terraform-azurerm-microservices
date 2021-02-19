b67682713728b5fa0ccb4da57aa9dcccf6b61d66
Josh Rolstad <jorolsta@microsoft.com>
2021-02-19 10:29:47 -0800

    passing owners through

--- modules/microservice/main.tf
+++ modules/microservice/main.tf
@@ -205,7 +205,7 @@ resource "azuread_application" "microservice" {
   prevent_duplicate_names    = true
   oauth2_allow_implicit_flow = true
   identifier_uris            = [lower("api://${local.full_microservice_environment_name}")]
-  owners                     = [var.azurerm_client_config.object_id]
+  owners                     = var.application_owners
   group_membership_claims    = "None"
   oauth2_permissions         = []
 
@@ -445,17 +445,20 @@ resource "azurerm_app_service" "microservice" {
     identity_ids = local.user_assigned_identity_ids
   }
 
-  auth_settings {
-    enabled = var.require_auth
-    active_directory  {
-         client_id = azuread_application.microservice.application_id
-         allowed_audiences = [ 
-           "https://${var.name}-${each.value.location}-${var.environment_name}${local.appservices_baseurl}",
-            local.microservice_trafficmanager_url 
-           ]
-     }
-     default_provider = "AzureActiveDirectory"
-     issuer = "https://sts.windows.net/${var.azurerm_client_config.tenant_id}"
+  dynamic "auth_settings" {
+    count = var.require_auth ? 1 : 0
+    content {
+      enabled = var.require_auth
+      active_directory  {
+          client_id = azuread_application.microservice.application_id
+          allowed_audiences = [ 
+            "https://${var.name}-${each.value.location}-${var.environment_name}${local.appservices_baseurl}",
+              local.microservice_trafficmanager_url 
+            ]
+      }
+      default_provider = "AzureActiveDirectory"
+      issuer = "https://sts.windows.net/${var.azurerm_client_config.tenant_id}"
+    }
   }
 }
 
