7ce97e943a5cb44bab93ae4ffaf03e5090a6046d
Matt Sanders <matthewsre@hotmail.com>
2021-03-03 12:27:42 -0800

    Changing default to use region short codes, will bump version from breaking change

--- variables.tf
+++ variables.tf
@@ -182,7 +182,7 @@ variable "regions" {
 variable "use_region_shortcodes" {
   description = "Use the shortened version of a region name in naming of resources."
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "azuread_instance" {
