39b64a44377fd3a7d782452bb1117a4c8dced7bf
Matt Sanders <matthewsre@hotmail.com>
2021-06-15 16:45:35 -0700

    Fixed to use specific version prior to breaking change in 2.58.0 https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/frontdoor_custom_https_configuration

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">=0.14, <0.15"
   experiments      = [module_variable_optional_attrs]
 
   required_providers {
     azurerm = {
-      version = ">= 2.0.0"
+      version = "=2.57.0"
       source  = "hashicorp/azurerm"
     }
   }
